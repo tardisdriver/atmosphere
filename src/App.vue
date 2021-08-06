0aedebe8f70e15d0a758a9f4413a5ef4fad8775d
RockitCat <thebesttardisdriver@gmail.com>
2021-08-05 20:06:41 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,76 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div style="display:flex; justify-content: center; align-items:center;">
+     <svg width="100%" height="100%" viewBox="0 0 400 200" id="rocket-path">
+       <circle cx="100" cy="100" r="80" />
+        <path d="
+            M 100, 100
+            m -75, 0
+            a 75,75 0 1,0 150,0
+            a 75,75 0 1,0 -150,0
+        "/>
+        <!-- <g id="rect">
+           <img id="rocket" src="./assets/rocket.png" />
+        </g> -->
+      </svg>
+      <img  id="rocket" src="./assets/rocket.png" />
+      <div id="globe-container" style="position:absolute">
+        <img alt="globe" id="globe" height="450px" width="450px" src="./assets/globe.png">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import { gsap } from "gsap"
+import { MotionPathPlugin } from "gsap/MotionPathPlugin.js";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  // components: {
+  //   HelloWorld
+  // }
+ mounted: function(){
+    gsap.registerPlugin(MotionPathPlugin);
+
+    gsap.timeline({
+      scrollTrigger:{
+        trigger: "#globe-container",
+        pin: false,
+        scrub:0.2,
+        start: 'top top',
+        end:'+=10000',
+      }
+    })
+    .to('#globe', {
+      rotation:360*5,
+      duration:1, ease:'none',
+    })
+    // gsap.to("#rocket", {
+    // duration: 5, 
+    // ease: "power1.inOut",
+    // immediateRender: true,
+    //   motionPath: {
+    //     path: "#rocket-path",
+    //     align: "#rocket-path",
+    //     alignOrigin: [0.5, 0.5],
+    //     autoRotate: 90
+    //   }
+    // });
+      gsap.to("#rocket", {
+      duration: 5, 
+      repeat: 12,
+      repeatDelay: 3,
+      yoyo: false,
+      ease: "power1.inOut",
+      motionPath:{
+        path: "#rocket-path",
+        align: "#rocket-path",
+        autoRotate: true,
+        alignOrigin: [0.5, 0.5]
+      }
+    });
   }
 }
 </script>
@@ -24,5 +83,18 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  height:1500px;
+}
+#globe-container {
+  margin-top:30px;
+  height: 455px;
+}
+#rocket {
+  transform: rotate(-40deg)
+}
+#rocket-path {
+  display:block;
+  margin-left:auto;
+  margin-right:auto;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import { gsap } from "gsap";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+import { TextPlugin } from "gsap/TextPlugin";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(MotionPathPlugin, TextPlugin, ScrollTrigger);
+
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
